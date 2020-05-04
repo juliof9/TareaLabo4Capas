9838fb8a2b7405d07942fbba22e3704bcd5d42cf
00081817@uca.edu.sv <00081817@uca.edu.sv>
2020-05-04 10:35:29 -0600

    Firts commit

---
+++ src/main/java/com/uca/TareaLabo4Capas/TareaLabo4CapasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.TareaLabo4Capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaLabo4CapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaLabo4CapasApplication.class, args);
+	}
+
+}
